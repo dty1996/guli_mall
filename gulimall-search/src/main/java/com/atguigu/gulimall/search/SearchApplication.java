b7686b2a892a9855102c3419167034a472c68b12
dty <dantangyao@hblg.vip>
2022-08-29 17:58:20 +0800

    es

---
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/SearchApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
---
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/SearchAppTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.gulimall.search;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchAppTest {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+    @Test
+    public void Test() {
+        System.out.println(restHighLevelClient);
+    }
+}
