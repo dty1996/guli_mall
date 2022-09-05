1b81f411b7c266d9e41e75c56267c1b0d63bf50f
dty <dantangyao@hblg.vip>
2022-09-05 17:01:38 +0800

    redis

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/config/RedissonConfig.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/RedissonConfig.java
@@ -7,7 +7,6 @@ import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
 
 /**
  * @author Administrator
@@ -17,11 +16,25 @@ import java.io.IOException;
 public class RedissonConfig {
     /**
      * 所有对Redisson的操作都通过redissonClient对象
+     *
+     * redisson特性：自动落锁 自动延长 阻塞式等待
+     *
+     *
+     * 在加锁时选择指定过期时间，手动解锁（加锁选择指定时间时，不进行自动续费，减少性能消耗）
+     *
+     *
+     * 读写锁 读锁共享锁 写锁互斥锁
+     * 读+读：无锁，并发读
+     * 写+写：阻塞方式
+     * 写+读：等待写锁
+     * 读+写：等待读锁
+     *
+     *
+     *
      * @return
-     * @throws IOException
      */
     @Bean(destroyMethod="shutdown")
-    RedissonClient redisson() throws IOException {
+    RedissonClient redisson() {
         Config config = new Config();
         //单节点
         config.useSingleServer()
